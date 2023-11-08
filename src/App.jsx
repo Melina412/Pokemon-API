d2893183c68e3390efaf728162c8bec36179ab18
SuperCoder <supercoder@AIRVONSPERCODER.local>
2023-11-08 21:53:34 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Import Page & Componets
-import Home from './page/Home';
-import DetailsPage from './page/DetailPage';
-import SearchPage from './page/SearchPage';
-import FetchData from './components/FetchData';
+import Home from "./page/Home";
+import DetailsPage from "./page/DetailPage";
+import SearchPage from "./page/SearchPage";
+import FetchData from "./components/FetchData";
 
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 // Import Context
-import { ThemeContext } from './Context/context';
-import { FetchContext } from './Context/context';
+import { ThemeContext } from "./Context/context";
+import { FetchContext } from "./Context/context";
 
 function App() {
   const [theme, setTheme] = useState(false);
   const [pokemon, setPokemon] = useState();
   return (
-    <>
-      <h1>HALLO WELT</h1>
-    </>
+    <section>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <FetchContext.Provider value={{ pokemon, setPokemon }}>
+          <BrowserRouter>
+            <FetchData />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/details" element={<DetailsPage />} />
+              <Route path="/search" element={<SearchPage />} />
+            </Routes>
+          </BrowserRouter>
+        </FetchContext.Provider>
+      </ThemeContext.Provider>
+    </section>
   );
 }
 
