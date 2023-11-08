9cd1c08341ac335f2aff77f7b3836aee2d5bbafa
Carina Klömpges <carinawerner25@gmail.com>
2023-11-08 16:26:32 +0100

    details

--- src/page/DetailPage.jsx
+++ src/page/DetailPage.jsx
@@ -1,23 +1,29 @@
 import SearchBox from "../components/SearchBox";
 
 export default function DetailPage() {
-
-const { pokemon, setPokemon } = useContext(FetchContext);
-console.log(pokemon);
+  const { pokemon, setPokemon } = useContext(FetchContext);
+  console.log(pokemon);
 
   return (
     <section>
       <img src="../../pokemon-img.png" alt="Logo" />
       <SearchBox />
-      <article className="coutries">
+      <article>
         {pokemon?.map((item, index) => (
           <div key={index}>
-            <img src={item.} alt="Pokemon" />
-            <h1>{item.name}</h1>
-            <hr />
-            <button>{item.}</button>
-
-          </div>))}
-        </article>
-    </section>);
+            <img
+              // oder item.sprites.other.home.front_default
+              src={item.sprites.other.official - artwork.front_default}
+              alt="Pokemon"
+            />
+            <h1>
+              {item.pokedex_numbers}
+              {item.names[5].name}
+            </h1>
+            <p>{item.habitat}</p>
+          </div>
+        ))}
+      </article>
+    </section>
+  );
 }
